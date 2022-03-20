804ed9b86a9122b290b9950e878e02ce2839325f
bxbalaban <balabanbsr@gmail.com>
2022-03-20 22:08:49 +0300

    tasks completed

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ App = {
         await App.loadAccount()
         await App.loadContract()
         await App.render()
+
     },
     loadWeb3: async () => {
         // Modern dapp browsers...
@@ -85,7 +86,7 @@ App = {
             $newTaskTemplate.find('input')
                             .prop('name',taskId)
                             .prop('checked',taskCompleted)
-                            // .on('click',App.toggleCompleted)
+                            .on('click',App.toggleCompleted)
             if(taskCompleted){
                 $('#completedTaskList').append($newTaskTemplate)
             }
@@ -99,6 +100,12 @@ App = {
         App.setLoading(true)
         const content = $('#newTask').val()
         await App.todoList.createTask(content, {from: App.account})
+        window.location.reload()
+    },
+    toggleCompleted: async (e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId, {from: App.account})
         window.location.reload()
     }
 }
